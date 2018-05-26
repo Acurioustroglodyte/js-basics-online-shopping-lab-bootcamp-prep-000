0599f4014eb1bfb22bb5a844e66dc461c69ade36
Trevor Jameson <tjameson15@gmail.com>
2018-05-26 14:28:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function total() {
 function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++) {
     if (cart[i]["itemName"] === item) {
-      splice()
+      splice(i, 0)
     }
   }
 }
